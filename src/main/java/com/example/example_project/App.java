d08bbe66af62b1a90eef24da92efd3c1c829b8a4
Baba <baba.sriharsha@gmail.com>
2023-12-30 20:02:19 +0530

    sample war

---
+++ src/main/java/com/example/example_project/App.java
@@ -0,0 +1,13 @@
+package com.example.example_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Nenu Amarudu ni ra ***" );
+    }
+}
